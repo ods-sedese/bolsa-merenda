e2deb7ae259671ae7b075d8e8a0ad5b3cd9530ef
Alexandre <alexandrefreitas92@gmail.com>
2020-12-03 14:57:37 -0300

    Atualizando município

--- helpers/transferencia_info.R
+++ helpers/transferencia_info.R
@@ -6,8 +6,9 @@ setwd("/srv/shiny-server/bolsa-merenda")
 # Read db
 pobre <- read_csv("data/Por_municipio_Nova_Sintese_Alunos_e_familias_pobres_contemplados_pelo_Bolsa_Merenda.csv")
 ext_pobre <- read_csv("data/Por_municipio_Nova_Sintese_Alunos_e_familias_ext_pobres_contemplados_pelo_Bolsa_Merenda.csv")
-mg_dados <- read.xlsx("data/mg_dados.xlsx")
-
+mg_dados <- read.xlsx("data/mg_dados.xlsx") %>%
+  select(Código.IBGE, Município) %>%
+  rename(`Código IBGE` = Código.IBGE)
 # Clean ext pobre
 ext_pobre <- ext_pobre %>%
   filter(!is.na(`Código IBGE`))
@@ -21,7 +22,10 @@ geral <- ext_pobre %>%
   select(3,1,2, 5, 7, 17, 24, 26, 36, 6, 9, 18, 25, 28, 37, 38, 19, 38) %>%
   mutate(across(everything(), ~replace_na(., 0)),
          `Valor repassado aos beneficiários` = `Total de dinheiro repassado aos beneficiários da lista atual de extremamente pobres` + total_dinheiro_lista_atual) %>%
-  select(-c(16, 17))
+  select(-c(16, 17)) %>%
+  left_join(mg_dados, by = "Código IBGE", suffix = c("", "_2")) %>%
+  mutate(Município = Município_2) %>%
+  select(-Município_2)
 
 # Change colnames
 col_names <- colnames(munic)
